ff3f37a2454033630a5f002f8482361f05c9b9ff
WeslleyAnd <jose_weslley_31@hotmail.com>
2022-04-24 22:29:09 -0300

    update consulta.py

--- consulta.py
+++ consulta.py
@@ -6,11 +6,11 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.google.com/")
-assert "Python" in driver.title
-elem = driver.find_element_by_id("q")
+driver.get("http://www.google.com")
+#assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
 elem.clear()
-elem.send_keys("pycon")
+elem.send_keys("José Weslley de Andrade")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 driver.close()
